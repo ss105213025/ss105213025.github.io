93b2ce35f18fa0f00039403a46e646c7a481264f
ss105213025 <u1055129@hk.edu.tw>
2022-05-10 21:11:28 +0800

    [push] 上傳完整檔案

---
+++ README.md
@@ -0,0 +1,9 @@
+<style>
+  font-family : 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+</style>
+
+# 個人履歷 - 介紹
+
+- 此靜態網頁為張嘉文本人設計之個人簡介
+
+### 使用技術
---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let main = document.querySelector('main');
+
+let lottie = document.querySelector('.kv');
+
+setTimeout(() => {
+  main.removeChild(lottie);
+}, 3500)
